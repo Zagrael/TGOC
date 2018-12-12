f06c367dfd8e0adeee14504d9b3cc52a0bcf90a1
Zagrael <aymerick-soyez@outlook.com>
2018-12-12 22:32:32 +0100

    Adding constructor for GRASP

--- GRASP.cpp
+++ GRASP.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "GRASP.h"
+
+GRASP::GRASP(const float &alpha, const int &n, const int F[][N_MAX], const int D[][N_MAX]) {
+
+    this->alpha = alpha;
+    this->n = n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            this->F[i][j] = F[i][j];
+            this->D[i][j] = D[i][j];
+        }
+    }
+}
--- GRASP.h
+++ GRASP.h
@@ -5,9 +5,22 @@
 #ifndef CHALLENGE_GRASP_H
 #define CHALLENGE_GRASP_H
 
+#ifndef  N_MAX
+const int N_MAX = 1000;
+#endif
 
 class GRASP {
 
+    float alpha;
+    int n;
+    int F[N_MAX][N_MAX];
+    int D[N_MAX][N_MAX];
+    int solution[N_MAX];
+    int objectiveValue;
+
+public:
+    // Constructor
+    GRASP(const float&, const int&, const int[][N_MAX], const int[][N_MAX]);
 };
 
 
