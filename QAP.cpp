310a1b689de961e3ee6d7ab5b458f6df1cebf145
Zagrael <aymerick-soyez@outlook.com>
2018-12-12 23:12:22 +0100

    Adding QAP class

---
+++ QAP.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Aymerick on 12-12-18.
+//
+
+#include "QAP.h"
+
+int QAP::computeObjectiveValue(const int &n, int solution[], int F[][N_MAX], int D[][N_MAX]) {
+    int value = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            value += D[i][j] * F[solution[i] - 1][solution[j] - 1];
+        }
+    }
+    return value;
+}
---
+++ QAP.h
@@ -0,0 +1,19 @@
+//
+// Created by Aymerick on 12-12-18.
+//
+
+#ifndef CHALLENGE_QAP_H
+#define CHALLENGE_QAP_H
+
+
+#include <iostream>
+#include "common.h"
+
+class QAP {
+
+public:
+    static int computeObjectiveValue(const int&, int[], int[][N_MAX], int[][N_MAX]);
+};
+
+
+#endif //CHALLENGE_QAP_H
