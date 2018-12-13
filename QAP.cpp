f1dde0f9614a859e8e014541459860eca525e013
Sabrine <sabrine.riahi@student.umons.ac.be>
2018-12-13 12:30:39 +0100

    Ajout QAP.cpp

--- QAP.cpp
+++ QAP.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "QAP.h"
+
+
+int QAP::computeObjectiveValue(const int &n, int solution[], int F[][N_MAX], int D[][N_MAX]) {
+    int value = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            value += D[i][j] * F[solution[i] - 1][solution[j] - 1];
+        }
+    }
+    return value;
+}
