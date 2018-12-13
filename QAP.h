12916831de68f1d9373074d59f72244550d84b6d
Sabrine <sabrine.riahi@student.umons.ac.be>
2018-12-13 12:32:49 +0100

    Ajout QAP.h

--- QAP.h
+++ QAP.h
@@ -5,9 +5,14 @@
 #ifndef CHALLENGE_QAP_H
 #define CHALLENGE_QAP_H
 
+#include <iostream>
+
+const int N_MAX = 1000;
 
 class QAP {
 
+public:
+    static int computeObjectiveValue(const int&, int[], int[][N_MAX], int[][N_MAX]);
 };
 
 
